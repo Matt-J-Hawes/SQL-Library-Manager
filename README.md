77fd66ceccdc70a6b0e91b4bb0f1416353beddd0
Matt Hawes <73285477+Matt-J-Hawes@users.noreply.github.com>
2021-02-03 12:28:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SQL-Library-Manager
+ App to stores books to a database and allows CRUD operations
---
+++ models/book.js
@@ -0,0 +1,39 @@
+'use strict';
+const { Model}  = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    static associate(models) {
+    }
+  };
+  Book.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Title is required!'
+        }
+      },
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Author is required!'
+        }
+      }
+    },
+    genre: {
+      type: DataTypes.STRING
+    },
+    year: {
+      type: DataTypes.INTEGER
+    }
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
